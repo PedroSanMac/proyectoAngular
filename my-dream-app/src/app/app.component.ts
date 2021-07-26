e46cb497cbef3e3def185ce5a99f407e7b12f17a
Pedr0 <psanchezm@unsa.edu.pe>
2021-07-26 12:58:03 +0200

    Se agrego el constructor y luego el boton para mostrar hobbies

--- my-dream-app/src/app/app.component.ts
+++ my-dream-app/src/app/app.component.ts
@@ -7,7 +7,26 @@ import { Component } from '@angular/core';
 })
 export class AppComponent {
   title = 'my-dream-app';
-  name = "Pedro";
-  email = "psanchezm@unsa.edu.pe";
-  webpage = "http://www.unsa.edu.pe";
+  name : string;
+  email;// = "psanchezm@unsa.edu.pe";
+  webpage : string;
+  hobbies : string[];
+  showHobbies : boolean;
+
+  constructor() {
+    console.log("Constructor trabajando...");
+    this.name = "Pedro";
+    this.email = "psanchezm@unsa.edu.pe";
+    this.webpage = "http://www.unsa.edu.pe";
+    this.hobbies = ["Futbol","Programacion","DOTA2"];
+    this.showHobbies = false;
+  }
+
+  toggleHobbies(){
+    this.showHobbies = !this.showHobbies;
+  }
+
+  showhobbies() {
+    return true;
+  }
 }
