81f99ecee6a2b7c773ec7db4e5505b579945d4d1
Pedr0 <psanchezm@unsa.edu.pe>
2021-07-26 21:39:49 +0200

    Agregada app user y hello-word

--- my-dream-app/src/app/app.component.ts
+++ my-dream-app/src/app/app.component.ts
@@ -6,6 +6,10 @@ import { Component } from '@angular/core';
   styleUrls: ['./app.component.css']
 })
 export class AppComponent {
+  users = ['ryan','joe','cameron','jhon'];
+  activated = false;
+  
+
   title = 'my-dream-app';
   name : string;
   email;// = "psanchezm@unsa.edu.pe";
