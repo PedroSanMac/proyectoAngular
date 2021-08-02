6b2edf5da15185f1c1e78897c1ebd7fc2ae5f991
Pedr0 <psanchezm@unsa.edu.pe>
2021-08-01 20:30:07 -0500

    Se agregaron los ruteos y aboutcomponent

--- my-dream-app/src/app/app.module.ts
+++ my-dream-app/src/app/app.module.ts
@@ -1,4 +1,4 @@
-import { NgModule } from '@angular/core';
+import { Component, NgModule } from '@angular/core';
 import { BrowserModule } from '@angular/platform-browser';
 
 import { AppRoutingModule } from './app-routing.module';
@@ -6,21 +6,31 @@ import { AppComponent } from './app.component';
 import { HelloWordComponent } from './hello-word/hello-word.component';
 import { UserComponent } from './user/user.component';
 import { FormsModule } from '@angular/forms';
+import { RouterModule, Route } from '@angular/router';
 
 import { HttpClientModule } from '@angular/common/http';
 import { DataService } from './data.service';
+import { AboutComponent } from './about/about.component';
+
+const routes: Route[] =[
+  {path: '', component: AppComponent},
+  {path: 'about', component:AboutComponent},
+  {path: 'hello', component: HelloWordComponent},
+];
 
 @NgModule({
   declarations: [
     AppComponent,
     HelloWordComponent,
-    UserComponent
+    UserComponent,
+    AboutComponent,
   ],
   imports: [
     BrowserModule,
     AppRoutingModule,
     FormsModule,
     HttpClientModule,
+    RouterModule.forRoot(routes),
   ],
   providers: [DataService],
   bootstrap: [AppComponent]
